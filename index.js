5e45e0fccfb0cbdcaa969afae7893bd552cd4f4a
Your Name~ <f.zihad333@gmail.com>
2023-02-01 20:37:38 +0600

    Post method Added

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+const users = [
+  {
+    id: 1,
+    name: "Rehody",
+    email: "rehody@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Reaz",
+    email: "Reaz@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Sakib",
+    email: "Sakib@gmail.com",
+  },
+];
+
+app.post("/addNew", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send(user);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello I am the Back End");
 });
+
+app.get("/members", (req, res) => {
+  res.send(users);
+});
+
 app.listen(port, () => {
   console.log("The Server is created and running on port 5000");
 });
